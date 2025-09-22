3d6e28debbec2b35de3a622971421440f64adf38
Sipos Richard <richard.sipos@sap.com>
2025-09-22 17:54:57 +0200

    Changes for hosting the service.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// Works for GitHub Pages under https://<user>.github.io/<repo>/
 export default defineConfig({
   plugins: [react()],
+  base: './',
 })
